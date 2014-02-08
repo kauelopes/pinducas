6ceef326ace98a068b5fed1223ca740709bb476d
Kaue <kauelopesmoraes22@gmail.com>
2014-02-08 01:27:32 -0200

    Commit para pegar atualizações do Gabriel

--- projeto_1-desktop/src/com/me/mygdxgame/Main.java
+++ projeto_1-desktop/src/com/me/mygdxgame/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "projeto_1";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.useGL20 = true;
+		cfg.width = 1920;
+		cfg.height = 1080;
 		
 		new LwjglApplication(new Core(), cfg);
 	}
